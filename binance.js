d96b74d6dbf2114f2355b9c3fe77d7b0386b4c63
larebsyed <lareb_khalil_syed@hotmail.com>
2018-01-08 16:13:58 +0500

    update

--- binance.js
+++ binance.js
@@ -1,14 +1,14 @@
 (function() {
-  main();
+    main();
 })();
+
 function main() {
     'use strict';
     var $trades = $("*[ng-show*='lswt'] #tradeOrderBox .table tr");
     var trades = [];
-    $trades.each(function()
-    {
+    $trades.each(function() {
         var trade = row_to_array(this);
-        trade = _.object(["date","pair","type","side","avg","price","filled","amount","total","status","row"], trade);
+        trade = _.object(["date", "pair", "type", "side", "avg", "price", "filled", "amount", "total", "status", "row"], trade);
         // _.invoke(trade, function(item){
         //     item.date = Moment(item.date, 'HH:mm:ss')
         // });
@@ -16,8 +16,10 @@ function main() {
     });
     var viewModel = {
         all_trades: [],
+        all_balances: [],
         selected_pair: selected_pair(),
         balance: ko.observable(0),
+        btc_balance: ko.observable(0),
         min_trade: ko.observable(0),
         upper_limit: ko.observable(0),
         lower_limit: ko.observable(0),
@@ -28,146 +30,240 @@ function main() {
         total_trades: ko.observable(0),
         duration: ko.observable(0),
         transaction_per_min: ko.observable(0),
-        update : function(){
-            this.duration(this.duration()+1);
-            if(_.isUndefined(this.all_trades[selected_pair()])){
+        cost_amount : ko.observable(0),
+        cost_usd : ko.observable(0),
+        update: function() {
+            this.all_balances = get_balances();
+            this.duration(this.duration() + 1);
+            if (_.isUndefined(this.all_trades[selected_pair()])) {
                 this.all_trades[selected_pair()] = [];
             }
             var new_trades = add_new_trades(this.all_trades[selected_pair()]);
-            if(new_trades.length>0){
-            this.all_trades[selected_pair()] = this.all_trades[selected_pair()].concat(new_trades);
-            var selected_trades = this.all_trades[selected_pair()]
-            var percentage1m = calculate_change(selected_trades,1).toFixed(2);
-            var percentage2m = calculate_change(selected_trades,2).toFixed(2);
-            var percentage3m = calculate_change(selected_trades,3).toFixed(2);
-            this.balance("$"+balance_to_usd().toFixed(5));
-            this.percentage1m(percentage1m+"%");
-            this.percentage2m(percentage2m+"%");
-            this.percentage3m(percentage3m+"%");
-            var stats = get_trade_stats(selected_trades);
-            this.max_trade(stats.max_trade.toFixed(8));
-            this.min_trade(stats.min_trade.toFixed(8));
-            this.upper_limit(stats.upper_quartile.toFixed(8));
-            this.lower_limit(stats.lower_quartile.toFixed(8));
-            this.total_trades(selected_trades.length);
-            times = _.pluck(selected_trades, "time");
-            var duration = _.max(times).diff(_.min(times), 'seconds');
-            this.duration(duration);
-            this.transaction_per_min((selected_trades.length/duration).toFixed(5));
-        }
+            if (new_trades.length > 0) {
+                this.all_trades[selected_pair()] = this.all_trades[selected_pair()].concat(new_trades);
+                var selected_trades = this.all_trades[selected_pair()];
+                var percentage1m = calculate_change(selected_trades, 1).toFixed(2);
+                var percentage2m = calculate_change(selected_trades, 2).toFixed(2);
+                var percentage3m = calculate_change(selected_trades, 3).toFixed(2);
+                var user_stats = get_user_trading_stats(selected_pair());
+                this.cost_amount(user_stats.amount.toFixed(5));
+                this.cost_usd(user_stats.usd.toFixed(5));
+                this.balance("$" + balance_to_usd().toFixed(5));
+                this.percentage1m(percentage1m + "%");
+                this.percentage2m(percentage2m + "%");
+                this.percentage3m(percentage3m + "%");
+                var stats = get_trade_stats(selected_trades);
+                this.max_trade(stats.max_trade.toFixed(5));
+                this.min_trade(stats.min_trade.toFixed(5));
+                this.upper_limit(stats.upper_quartile.toFixed(5));
+                this.lower_limit(stats.lower_quartile.toFixed(5));
+                this.total_trades(selected_trades.length);
+                times = _.pluck(selected_trades, "time");
+                var duration = _.max(times).diff(_.min(times), 'seconds');
+                this.duration(duration);
+                this.transaction_per_min((selected_trades.length / duration).toFixed(5));
+            }
         },
-        reset: function(){
-            this.all_trades[selected_pair()]=get_public_trades();
+        reset: function() {
+            this.all_trades[selected_pair()] = get_public_trades();
         }
     };
+    create_html();
     viewModel.all_trades[selected_pair()] = get_public_trades();
     setInterval(function() {
         viewModel.update();
     }, 1000);
-   
-    create_html();
+
     ko.applyBindings(viewModel, $("#stats")[0]);
 
-    function create_html(){
+    function create_html() {
         var table = emmet.make("table.table#stats>tbody>tr>th>button[data-bind=click:reset()]{reset}^+th{Max}+th{Min}");
-        var row = emmet.make("tr>th{Price}+td[data-bind=text:max_trade]+td[data-bind=text:min_trade]");
-        var row1 = emmet.make("tr>th{Buy Limit}+td[data-bind=text:upper_limit]+td[data-bind=text:lower_limit]");
-        var row2 = emmet.make("tr>th{1 min % +/-}+td+td[data-bind=text:percentage1m]");
-        var row3 = emmet.make("tr>th{2 min % +/-}+td+td[data-bind=text:percentage2m]");
-        var row4 = emmet.make("tr>th{3 min % +/-}+td+td[data-bind=text:percentage3m]");
-        var row5 = emmet.make("tr>th{Total Trades}+td+td[data-bind=text:total_trades]");
-        var row6 = emmet.make("tr>th{T/S}+td+td[data-bind=text:transaction_per_min]");
-        var row7 = emmet.make("tr>th{Duration}+td+td[data-bind=text:duration]");
-        var row8 = emmet.make("tr>th{Balance}+td+td[data-bind=text:balance]");
-        $(table).append($(row));
-        $(table).append($(row1));
-        $(table).append($(row2));
-        $(table).append($(row3));
-        $(table).append($(row4));
-        $(table).append($(row5));
-        $(table).append($(row6));
-        $(table).append($(row7));
-        $(table).append($(row8));
+        var rows = [];
+        rows.push(emmet.make("tr>th{Price}+td[data-bind=text:max_trade]+td[data-bind=text:min_trade]"));
+        rows.push(emmet.make("tr>th{Buy Limit}+td[data-bind=text:upper_limit]+td[data-bind=text:lower_limit]"));
+        rows.push(emmet.make("tr>th{1 min % +/-}+td+td[data-bind=text:percentage1m]"));
+        rows.push(emmet.make("tr>th{2 min % +/-}+td+td[data-bind=text:percentage2m]"));
+        rows.push(emmet.make("tr>th{3 min % +/-}+td+td[data-bind=text:percentage3m]"));
+        rows.push(emmet.make("tr>th{Total Trades}+td+td[data-bind=text:total_trades]"));
+        rows.push(emmet.make("tr>th{T/S}+td+td[data-bind=text:transaction_per_min]"));
+        rows.push(emmet.make("tr>th{Duration}+td+td[data-bind=text:duration]"));
+        rows.push(emmet.make("tr>th{Current Balance}+td{}+td[data-bind=text:balance]"));
+        rows.push(emmet.make("tr>th{Cost Amount/USD}+td[data-bind=text:cost_amount]+td[data-bind=text:cost_usd]"));
+        _.invoke(rows, function(){
+            $(table).append($(this));
+        });
         $("#tradeHistory .table:first").before(table);
     }
-    function calculate_change(trades, min){
-        console.log("trades",trades);
-        if(trades.length>0){
+
+    function calculate_change(trades, min) {
+        console.log("trades", trades);
+        if (trades.length > 0) {
             var now = _.max(_.pluck(trades, "time"));
-            var last_minute_trades =_.filter(trades, function(trade){
-                                                        if(min*60 >= now.diff(trade.time,"seconds"))
-                                                            return true;                
-                                                    });
-            last_minute_trades = _.sortBy(last_minute_trades, function(trade){return trade.time;});
+            var last_minute_trades = _.filter(trades, function(trade) {
+                if (min * 60 >= now.diff(trade.time, "seconds"))
+                    return true;
+            });
+            last_minute_trades = _.sortBy(last_minute_trades, function(trade) {
+                return trade.time;
+            });
             var prices = _.pluck(last_minute_trades, "price");
 
             var last_price = parseFloat(prices[0]);
-            var current_price = parseFloat(prices[prices.length-1]);
-            return ((current_price-last_price)/last_price)*100;
+            var current_price = parseFloat(prices[prices.length - 1]);
+            return ((current_price - last_price) / last_price) * 100;
         }
         return 0;
     }
-    function get_trade_stats(public_trades){
-        var prices = _.map(public_trades, function(trade){return parseFloat(trade.price.replace(',',''));});
+
+    function get_trade_stats(public_trades) {
+        var prices = _.map(public_trades, function(trade) {
+            return parseFloat(trade.price.replace(',', ''));
+        });
         var stats = {
-                    min_trade: nj.min(prices),
-                    mean_trade: nj.mean(prices),
-                    max_trade: nj.max(prices),
-                    upper_quartile: nj.mean(prices) + nj.std(prices),
-                    lower_quartile: nj.mean(prices) - nj.std(prices),
-                    spread: nj.max(prices)-nj.min(prices)
-                };
+            min_trade: nj.min(prices),
+            mean_trade: nj.mean(prices),
+            max_trade: nj.max(prices),
+            upper_quartile: nj.mean(prices) + nj.std(prices),
+            lower_quartile: nj.mean(prices) - nj.std(prices),
+            spread: nj.max(prices) - nj.min(prices)
+        };
         return stats;
     }
-    function get_public_trades(){
-        debugger;
-        var loading = $("#tradeHistory #divLoading").filter(function () { 
-            return $(this).css("display") != 'none'; 
+
+    function get_user_trading_stats(selected_pair) {
+        var user_trades = get_user_trades(selected_pair);
+        var balance = current_balance();
+        var usd_balance = balance_to_usd();
+        var btc_balance = balance_to_btc();
+        var usd_to_btc = usd_balance / btc_balance;
+        var trade_btc = 0;
+        var trade_balance = 0;
+        for (var trade of _(user_trades).where({"status":"Filled"}))
+        {
+            if(trade_balance < balance)
+            {
+                if(trade.side == "Sell")
+                {
+                    trade_balance -= parseFloat(trade.amount);
+                    trade_btc -= parseFloat(trade.total);
+                }
+                if(trade.side == "Buy")
+                {
+                    if(parseFloat(trade.amount)+trade_balance>balance)
+                    {
+                        var difference = balance - trade_balance;
+                        trade_btc += difference * parseFloat(trade.price);
+                        trade_balance = balance;
+                    }
+                    else{
+                        trade_balance += parseFloat(trade.amount);
+                        trade_btc += parseFloat(trade.total);
+                    }
+                }
+
+            }
+        }
+
+        var base_currency = selected_pair.split("/")[1];
+        var holding={
+            "current_amount": balance,
+            "current_usd" : usd_balance,
+            "current_btc" : btc_balance,
+            "amount" : trade_balance,
+            "btc" : (base_currency == "USDT") ? trade_btc/usd_to_btc :  trade_btc,
+            "usd" : (base_currency == "USDT") ? trade_btc :  trade_btc*usd_to_btc
+        };
+        return holding;
+    }
+    function get_user_trades(pair){
+        var $trades = $("*[ng-show*='lswt'] #tradeOrderBox .table tr");
+        var trades = [];
+        $trades.each(function() {
+        var trade = row_to_array(this);
+        trade = _.object(["date", "pair", "type", "side", "avg", "price", "filled", "amount", "total", "status", "row"], trade);
+        trades.push(trade);
         });
-        if(loading.length>0)
+        return _(trades).where({"pair": pair});
+    }
+    function get_public_trades() {
+        var loading = $("#tradeHistory #divLoading").filter(function() {
+            return $(this).css("display") != 'none';
+        });
+        if (loading.length > 0)
             return [];
-        var public_trades=[];
-        var $public_trades =  $("#tradeHistory .newtrade tr");
-        $public_trades.each(function(){
+        var public_trades = [];
+        var $public_trades = $("#tradeHistory .newtrade tr");
+        $public_trades.each(function() {
             var trade = row_to_array(this);
-            trade = _.object(["price","amount","time", "row"], trade);
+            trade = _.object(["price", "amount", "time", "row"], trade);
             trade.time = moment(trade.time, 'HH:mm:ss');
             public_trades.push(trade);
         });
-        return public_trades; 
+        return public_trades;
     }
-    function selected_pair(){
+
+    function get_balances() {
+        var $balances = $(".zjcc_table tr");
+        var balances = [];
+        $balances.each(function() {
+            var balance = row_to_array(this);
+            balance = _.object(["coin", "total_balance", "available_balance", "inorder", "btc", "row"], balance);
+            balances.push(balance);
+        });
+        return balances;
+    }
+
+    function get_currency() {
+        var currency = selected_pair().split('/')[0];
+        return currency;
+    }
+    function selected_pair() {
         return $(".selectsym[ng-class*='isOpened'] span:first").text();
     }
-    function balance_to_usd(){
-            var currency = selected_pair().split('/')[0];
-            var usd_rate = $(".transMoney").text();
-            usd_rate=usd_rate.replace("$","");
-            usd_rate=usd_rate.replace(",","");
-            usd_rate=usd_rate.replace(currency, '');
-            usd_rate = parseFloat(usd_rate);
-            var balance = $(".orderform div:not(.ng-hide) .f-fr .bid-div .f-fr").text();
-            balance = parseFloat(balance.replace(currency, '').replace(' ',''));
-            return balance*usd_rate;
+
+    function balance_to_usd() {
+        var currency = get_currency();
+        var usd_rate = $(".transMoney").text();
+        usd_rate = usd_rate.replace("$", "");
+        usd_rate = usd_rate.replace(",", "");
+        usd_rate = usd_rate.replace(currency, '');
+        usd_rate = parseFloat(usd_rate);
+        var balance = current_balance();
+        return balance * usd_rate;
     }
-    function add_new_trades(all_trades)
-    {
+    function balance_to_btc() {
+        var currency = get_currency();
+        var btc_rate = $(".table.depthbg .ng-binding:first").text();
+        btc_rate = parseFloat(btc_rate);
+        var balance = current_balance();
+        return balance * btc_rate;
+    }
+    function current_balance() {
+         var currency = get_currency();
+         var balance = $(".orderform div:not(.ng-hide) .f-fr .bid-div .f-fr").text();
+         balance = parseFloat(balance.replace(currency, '').replace(' ', ''));
+         return balance;
+    }
+
+    function add_new_trades(all_trades) {
         var new_trades = get_public_trades();
         var times = _.pluck(all_trades, "time");
         var max_time = _.max(times);
-        new_trades =_.filter(new_trades, function(trade){
-                                            if(max_time < trade.time)
-                                                return true;                
-                                        });
+        new_trades = _.filter(new_trades, function(trade) {
+            if (max_time < trade.time)
+                return true;
+        });
         return new_trades;
     }
-    function row_to_array(row){
+
+    function row_to_array(row) {
         var $trade = $(row).find(".ng-binding");
         var trade = [];
-        $trade.each(function(){
-           trade.push($(this).html());
+        $trade.each(function() {
+            trade.push($(this).html());
         });
         trade.push(row);
         return trade;
     }
-};
+
+}

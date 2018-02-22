3b5928da69396b33bab450782432fdb95113ac2d
larebsyed <lareb_khalil_syed@hotmail.com>
2018-02-22 18:38:26 +0500

    Make Repository usable:
    1. Added tampermonkey script to repo
    2. Added readme

---
+++ binance.tampermonkey.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Binance
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.binance.com/tradeDetail.html*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/numjs/0.15.1/numjs.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min.js
+// @require      https://raw.githubusercontent.com/JamesRobertNgo/EmmetJS/master/emmet.js
+// @require      https://raw.githubusercontent.com/larebsyed/Binance/master/binance.js
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
---
+++ readme.md
@@ -0,0 +1,37 @@
+
+# Table of Contents
+
+1.  [Binance Advance Mode Userscript](#org889316e)
+    1.  [Features](#org67ed09a)
+    2.  [Usage](#orgc9485ed)
+
+
+<a id="org889316e"></a>
+
+# Binance Advance Mode Userscript
+
+
+<a id="org67ed09a"></a>
+
+## Features
+
+1.  Max Min Price Range
+2.  Buy Limit (Prices Safe to buy sell)
+3.  1 min % change
+4.  2 min % change
+5.  3 min % change
+6.  Total Trade Since Page Visit
+7.  Trade per Second since page visit
+8.  Duration since page visit
+9.  Current Balance for the visited currency (Logged in)
+10. Cost of current balance in $
+11. Auto Update latest buy and sell
+
+
+<a id="orgc9485ed"></a>
+
+## Usage
+
+1.  Install the tampermonkey script
+2.  Open Binance Advance Trading Page
+
